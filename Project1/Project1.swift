5a31cb9ba7102f323803269700fa65eee4666f12
Fred Lee <lfredl109@gmail.com>
2015-09-11 21:39:46 -0700

    done part 1

--- Project1/Project1.swift
+++ Project1/Project1.swift
@@ -1,7 +1,34 @@
 func nextCellStates(currentStates: [[Bool]]) -> [[Bool]] {
     // IMPLEMENT ME
     // Take a look at https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for the rules
-    return currentStates
+    func isValid(row: Int, col: Int) -> Bool {
+        return (row >= 0 && row < currentStates.count && col >= 0 && col < currentStates[0].count)
+    }
+    var nextState: [[Bool]] = currentStates
+    for i in 0..<nextState.count {
+        for j in 0..<nextState[0].count {
+            var liveCellCount: Int = 0
+            for k in (i - 1)...(i + 1) {
+                for l in (j - 1)...(j + 1) {
+                    if (isValid(k, col: l) && !(k == i && l == j)) {
+                        if (currentStates[k][l] == true) {
+                            liveCellCount++
+                        }
+                    }
+                }
+            }
+            if (liveCellCount < 2) {
+                nextState[i][j] = false;
+            } else if (liveCellCount == 2 && liveCellCount == 3 && currentStates[i][j]) {
+                continue
+            } else if (liveCellCount > 3) {
+                nextState[i][j] = false;
+            } else if (liveCellCount == 3 && !currentStates[i][j]) {
+                nextState[i][j] = true;
+            }
+        }
+    }
+    return nextState
 }
 
 class LRUCache<K:Hashable, V> {
